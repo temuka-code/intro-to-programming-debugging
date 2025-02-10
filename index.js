ad78aec7f6d95233847f0e2fb29235da7e0e320f
Budkas <budkas@Budkass-MacBook-Pro.local>
2025-02-09 20:52:15 -0700

    added safeguard on number guess ,undo deletion  in setup function

--- index.js
+++ index.js
@@ -25,6 +25,11 @@ function getRandomNumber(min, max) {
 function checkGuess() {
   // Get value from guess input element
   const guess = parseInt(guessInput.value, 10);
+  if ( guess < 1 || guess > 99) {
+    alert("Please enter a number between 1 and 99");
+    return;
+  }
+
   attempts = attempts + 1;
 
   hideAllMessages();
@@ -84,7 +89,7 @@ function setup() {
   guessInput.disabled = false;
 
   hideAllMessages();
-  resetButton.style.display = '';
+  resetButton.style.display = 'none';
 }
 
 submitButton.addEventListener('click', checkGuess);
